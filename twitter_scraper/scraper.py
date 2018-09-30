95af817a15870bb632f1a36f3e36dbf24ddae51c
Andrew Cunningham <andrewcunnin@umass.edu>
2018-09-29 23:40:35 -0400

    add documentation, word probability,  update convention

--- twitter_scraper/scraper.py
+++ twitter_scraper/scraper.py
@@ -3,31 +3,31 @@ import re
 import json
 import nltk
 
-def removeTweetElems(str): #removes tweet elements like hashtags, RTs, and links
+def remove_tweet_elems(str_var): #removes tweet elements like hashtags, RTs, and links
 	regex = re.compile("^(RT)|(@[^\s]*\s?)|(#[^\s]*\s?)|(https://[^\s]*[\s]?)*")
-	return regex.sub('', str);
+	return regex.sub('', str_var);
     
-def cleanCorpus(str):
-	return removePunctuation(removeTweetElems(removeStops(str))).lower();
+def clean_corpus(str_var): #cleans everything except words and spaces from str_varing
+	return remove_punctuation(remove_tweet_elems(remove_stops(str_var))).lower();
 
-def removePunctuation(str):
+def remove_punctuation(str_var): #removes all punctuation from str_varing
 	regex = re.compile("[^a-z\sA-Z0-9]*")
-	return regex.sub('', str);
+	return regex.sub('', str_var);
 
-def removeWord(word, str):
+def remove_word(word, str_var): #removes all occurrences of word from str_varing
 	regex = re.compile("\s([^A-Z|a-z|0-9]*)" + word + "([^A-Z|a-z|0-9]*)\s")
-	return regex.sub(' ', str)
+	return regex.sub(' ', str_var)
 
-def removeStops(str):
+def remove_stops(str_var): #removes stop words from str_varing
 	stops = ["a", "also", "an", "and", "as", "at", "be", "but", "by", "can", "could", "do", "for", "from", "go", "have",
 	     "her", "here", "his", "how", "i", "if", "in", "into", "it", "its", "my", "of", "on", "or", "our", "say", "she",
 	     "that", "the", "their", "there", "therefore", "they", "this", "these", "those", "through", "to", "until", "we",
 	     "what", "when", "where", "which", "while", "who", "with", "would", "you", "your"]
 	for stop in stops:
-		str = removeWord(stop, str)
-	return str;
+		str_var = remove_word(stop, str_var)
+	return str_var;
 
-def scrapeNoisy(keyTerm, n):
+def scrape_noisy(keyTerm, n): #does not parse tweets, term to search by, amount of tweets to scrape
 	if(n > 100):
 		n = 100
 	if(n < 0):
@@ -35,43 +35,28 @@ def scrapeNoisy(keyTerm, n):
 	tweets = system.api.search(q=keyTerm, count = n)
 	return [getattr(tweet, 'text').encode('UTF-8') for tweet in tweets]
 
-def scrape(keyTerm, n):
-    tweets = scrapeNoisy(keyTerm, n)
-    return [removeTweetElems(tweet) for tweet in tweets]
+def scrape(keyTerm, n): #term to search by, amount of tweets to scrape
+    tweets = scrape_noisy(keyTerm, n)
+    return [remove_tweet_elems(tweet) for tweet in tweets]
 
-
-#deprecated old frequency implementation, replaced with NLTK
-#def getWordFrequencies(str):
- #   freqs = {}
-  #  strs = str.split()
-   # for word in strs:
-    #    if(word in freqs):
-     #       freqs[word] += 1
-      #  else:
-       #     freqs[word] = 1
-    #return freqs
-
-#def getRelativeFrequencies(contents):
- #   rel_freqs = {}
-  #  count = 0
-   # for key, value in contents.items():
-    #    count += value
-    #for key, value in contents:
-     #   rel_freqs[key] = float(value/count)
-
-def writeCorpus(keyword_list):
+def write_corpus(keyword_list): #writes all tweet bodies to json given search terms
 	tweets = []
 	for keyWord in keyword_list:
 		tweets += system.api.search(q=keyWord, count = 100)
 	with open('my_tweet_corpus.json', 'w') as outfile:
 		(json.dump(tweet, outfile) for tweet in tweets)
 
-def getCorpus(keyWord):
+def get_probability(str_var, substring): #finds probability that substr_varing is found in str_varing
+	arr = nltk.word_tokenize(str_var)
+	fdist = nltk.FreqDist(arr)
+	return fdist[substring]/len(arr) #should store distribution as a field for parse objects
+
+def get_corpus(keyWord): #scrapes a default 100 tweets given a search term
 	tweets = scrape(keyWord, 100)
 	if (tweets != None):
-		str = ""
+		str_var = ""
 		for tweet in tweets:
-			str += tweet + " "
-		return(str)
-
-print(nltk.word_tokenize(cleanCorpus("u'RT u'and \ni, must, 1 say that this is a nice house RT @user #cool https://mywebsite.com haha'")))
+			str_var += tweet + " "
+		return(str_var)
+str_var = "hi hi hi this is me ok"
+print(str(get_probability(str_var, "hi")))
